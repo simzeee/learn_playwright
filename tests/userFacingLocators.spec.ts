95181d53d141466fa1fa9676da9546b10821d9fd
John Sims <johnsims@Johns-Mac-mini.local>
2025-01-05 14:03:39 -0500

    finish child and parent locators

--- tests/userFacingLocators.spec.ts
+++ tests/userFacingLocators.spec.ts
@@ -7,7 +7,8 @@ test.beforeEach(async ({ page }) => {
 });
 
 async function navigateToFormLayouts(page) {
-  await page.goto("localhost:4200/");
+  console.log("ran");
+  await page.goto("http://localhost:4200/");
   await page.getByText("Forms").click();
   await page.getByText("Form Layouts").click();
 }
@@ -30,4 +31,71 @@ test("User facing locators", async ({ page }) => {
 
   await page.getByTestId("SignIn").waitFor({ state: "visible" });
   await page.getByTestId("SignIn").click();
+});
+
+// child selectors
+
+test("Using child selectors", async ({ page }) => {
+  // space
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+  // chain locators
+  await page
+    .locator("nb-card")
+    .locator("nb-radio")
+    .locator(":text-is('Option 1')")
+    .click();
+
+  // combine regular locator with user-facing locator
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "Sign in" })
+    .first()
+    .click();
+
+  // least preferable, nth-element
+  await page.locator("nb-card").nth(3).getByRole("button").click();
+});
+
+// parent elements
+
+test("selecting parent elements", async ({ page }) => {
+  // this example will return one element
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic Form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator(".status-danger") })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+
+  // we want to find the email input field for a particular form
+
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "sign-in" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+
+  // xPath, but not recommended
+
+  await page
+    .locator(':text-is("Using the Grid")')
+    .locator("..")
+    .getByRole("textbox", { name: "Email" })
+    .click();
 });
